e32a37970c1eb8eb863cbe38f8d977cbe43108e1
karim Ashraf <karimashraf000@gmail.com>
2025-02-20 10:27:21 +0200

    LazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/pages/home/home.component';
-import { ProductsComponent } from './features/pages/products/products.component';
-import { CategoriesComponent } from './features/pages/categories/categories.component';
-import { BrandsComponent } from './features/pages/brands/brands.component';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
-import { NotFoundComponent } from './features/pages/not-found/not-found.component';
-import { ForgetPasswordComponent } from './features/auth/forget-password/forget-password.component';
 import { authGuard } from './core/guards/auth/auth.guard';
-import { CheckoutComponent } from './features/pages/checkout/checkout.component';
-import { VerifyResetCodeComponent } from './features/auth/verify-reset-code/verify-reset-code.component';
-import { ResetPasswordComponent } from './features/auth/reset-password/reset-password.component';
-import { ProductDetailsComponent } from './features/pages/product-details/product-details.component';
-import { CartComponent } from './features/pages/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, title: 'Home' },
-  { path: 'products', component: ProductsComponent, title: 'Products', canActivate: [authGuard] },
-  { path: 'categories', component: CategoriesComponent, title: 'Categories', canActivate: [authGuard] },
-  { path: 'brands', component: BrandsComponent, title: 'Brands', canActivate: [authGuard] },
-  { path: 'checkout', component: CheckoutComponent, title: 'Check-Out', canActivate: [authGuard] },
-  { path: 'product-details/:id', component: ProductDetailsComponent, title: 'Product Details', canActivate: [authGuard] },
-  { path: 'cart', component: CartComponent, title: 'Cart Of Products', canActivate: [authGuard] },
-  { path: 'register', component: RegisterComponent, title: 'Register' },
-  { path: 'login', component: LoginComponent, title: 'Login' },
-  { path: 'forget-password', component: ForgetPasswordComponent, title: 'Forget Password' },
-  { path: 'verify-reset-code', component: VerifyResetCodeComponent, title: 'Reset Code Validation' },
-  { path: 'reset-password', component: ResetPasswordComponent, title: 'Change Password' },
-  { path: '**', component: NotFoundComponent },
+  { path: 'products', title: 'Products', canActivate: [authGuard], loadComponent: () => import('./features/pages/products/products.component').then(c => c.ProductsComponent) },
+  { path: 'categories', title: 'Categories', canActivate: [authGuard], loadComponent: () => import('./features/pages/categories/categories.component').then(c => c.CategoriesComponent) },
+  { path: 'brands', title: 'Brands', canActivate: [authGuard], loadComponent: () => import('./features/pages/brands/brands.component').then(c => c.BrandsComponent) },
+  { path: 'checkout', title: 'Checkout', canActivate: [authGuard], loadComponent: () => import('./features/pages/checkout/checkout.component').then(c => c.CheckoutComponent) },
+  { path: 'product-details/:id', title: 'Product Details', canActivate: [authGuard], loadComponent: () => import('./features/pages/product-details/product-details.component').then(c => c.ProductDetailsComponent) },
+  { path: 'cart', title: 'Cart Of Products', canActivate: [authGuard], loadComponent: () => import('./features/pages/cart/cart.component').then(c => c.CartComponent) },
+  { path: 'register', title: 'Register', loadComponent: () => import('./features/auth/register/register.component').then(c => c.RegisterComponent) },
+  { path: 'login', title: 'Login', loadComponent: () => import('./features/auth/login/login.component').then(c => c.LoginComponent) },
+  { path: 'forget-password', title: 'Forget Password', loadComponent: () => import('./features/auth/forget-password/forget-password.component').then(c => c.ForgetPasswordComponent) },
+  { path: 'verify-reset-code', title: 'Reset Code Validation', loadComponent: () => import('./features/auth/verify-reset-code/verify-reset-code.component').then(c => c.VerifyResetCodeComponent) },
+  { path: 'reset-password', title: 'Change Password', loadComponent: () => import('./features/auth/reset-password/reset-password.component').then(c => c.ResetPasswordComponent) },
+  { path: '**', loadComponent: () => import('./features/pages/not-found/not-found.component').then(c => c.NotFoundComponent) },
 ];
