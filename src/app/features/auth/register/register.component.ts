6e8621272ea12420b5d98b6e92f7fefdf5af4d54
karimprime <karimashraf000@gmail.com>
2025-02-27 01:20:38 +0200

    Edit Register

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,13 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { Router, RouterLink } from '@angular/router';
 import { Subscription } from 'rxjs';
 
+import { TranslatePipe } from '@ngx-translate/core';
+import { PasswordCheckLabelPipe } from '../../../shared/pipes/passwordCheckLabel/password-check-label.pipe';
+
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, CommonModule, RouterLink],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink, TranslatePipe, PasswordCheckLabelPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -16,11 +19,22 @@ export class RegisterComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
 
+  errorMessage = signal<string>('');
+  isLoading = signal<boolean>(false);
+  passwordVisibility = signal<{ password: boolean }>({ password: false });
+  RePasswordVisibility = signal<{ rePassword: boolean }>({ rePassword: false });
+  passwordFocused = signal<boolean>(false);
+
+  // Password Strength Checks using Signals
+  passwordChecks = signal({
+    hasUppercase: false,
+    hasLowercase: false,
+    hasNumber: false,
+    hasMinLength: false,
+    hasSpecial: false
+  });
+
   constructor() { }
-
-  errorMessage: string = "";
-
-  isLoading: boolean = false;
 
   registerSub: Subscription = new Subscription();
 
@@ -33,42 +47,33 @@ export class RegisterComponent {
     agree: new FormControl(false, [Validators.requiredTrue])
   }, { validators: this.confirmPassword.bind(this) });
 
-  passwordVisibility: Record<"password" | "rePassword", boolean> = {
-    password: false,
-    rePassword: false
-  };
-
-  passwordChecks = {
-    hasUppercase: false,
-    hasLowercase: false,
-    hasNumber: false,
-    hasMinLength: false,
-    hasSpical: false
-  };
-
-  passwordFocused = false;
 
   get passwordControl(): FormControl {
     return this.registerForm.get('password') as FormControl;
   }
 
 
-  togglePasswordVisibility(inputEl: HTMLInputElement, field: "password" | "rePassword"): void {
-    this.passwordVisibility[field] = !this.passwordVisibility[field];
-    inputEl.type = this.passwordVisibility[field] ? 'text' : 'password';
+  togglePasswordVisibility(inputEl: HTMLInputElement, field: 'password'): void {
+    this.passwordVisibility.set({ ...this.passwordVisibility(), [field]: !this.passwordVisibility()[field] });
+    inputEl.type = this.passwordVisibility()[field] ? 'text' : 'password';
   }
 
+  toggleRePasswordVisibility(inputEl: HTMLInputElement, field: 'rePassword'): void {
+    this.RePasswordVisibility.set({ ...this.RePasswordVisibility(), [field]: !this.RePasswordVisibility()[field] });
+    inputEl.type = this.RePasswordVisibility()[field] ? 'text' : 'password';
+  }
+
+
   updatePasswordStrength(): void {
-    const password = this.passwordControl.value || "";
-    this.passwordChecks = {
+    const password = this.passwordControl.value || '';
+    this.passwordChecks.set({
       hasUppercase: /[A-Z]/.test(password),
       hasLowercase: /[a-z]/.test(password),
       hasNumber: /\d/.test(password),
-      hasSpical: /[@$!%*?&]/.test(password),
+      hasSpecial: /[@$!%*?&]/.test(password),
       hasMinLength: password.length >= 8
-    };
+    });
   }
-
   confirmPassword(control: AbstractControl) {
     const password = control.get('password')?.value;
     const rePassword = control.get('rePassword')?.value;
@@ -86,7 +91,7 @@ export class RegisterComponent {
   }
 
   RegisterSubmit() {
-    this.isLoading = true;
+    this.isLoading.set(true);
     this.registerForm.markAllAsTouched();
 
     if (this.registerForm.valid) {
@@ -95,12 +100,12 @@ export class RegisterComponent {
           if (res.message === "success") {
             this.router.navigate(['/auth/login']);
           }
-          this.isLoading = false;
+          this.isLoading.set(false);
 
         },
       });
     } else {
-      this.isLoading = false;
+      this.isLoading.set(false);
     }
   }
 
